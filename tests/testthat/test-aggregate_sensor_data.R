4d9bfe7b84894c9ba7e655ea56745b2cac3f97b0
Liz Roten <liz.roten@metc.state.mn.us>
2020-07-07 17:12:52 -0500

    style

--- tests/testthat/test-aggregate_sensor_data.R
+++ tests/testthat/test-aggregate_sensor_data.R
@@ -26,23 +26,29 @@ testthat::try_again(2, {
     }
 
     # test aggregation at 15 minutes----------------------------------------------
-    agg <- aggregate_sensor_data(sensor_results, interval_length = 0.25,
-                                 config = config_sample)
+    agg <- aggregate_sensor_data(sensor_results,
+      interval_length = 0.25,
+      config = config_sample
+    )
     testthat::expect_equal(dim(agg)[[1]], 96)
     testthat::expect_equal(sum(sensor_results$volume), sum(agg$volume.sum))
     testthat::expect_equal(sum(sensor_results$occupancy), sum(agg$occupancy.sum))
 
     # test aggregation at 1 hour--------------------------------------------------
-    agg_hour <- aggregate_sensor_data(sensor_results, interval_length = 1,
-                                      config = config_sample)
+    agg_hour <- aggregate_sensor_data(sensor_results,
+      interval_length = 1,
+      config = config_sample
+    )
     testthat::expect_equal(dim(agg_hour)[[1]], 24)
     testthat::expect_equal(sum(sensor_results$volume, na.rm = T), sum(agg_hour$volume.sum))
     testthat::expect_equal(sum(sensor_results$occupancy, na.rm = T), sum(agg_hour$occupancy.sum))
     testthat::expect_lt(mean(agg$speed) - mean(agg_hour$speed), 1)
 
     # test aggregation at 24 hours------------------------------------------------
-    agg_day <- aggregate_sensor_data(sensor_results, interval_length = 24,
-                                     config = config_sample)
+    agg_day <- aggregate_sensor_data(sensor_results,
+      interval_length = 24,
+      config = config_sample
+    )
     testthat::expect_equal(dim(agg_day)[[1]], 1)
     testthat::expect_equal(sum(sensor_results$volume, na.rm = T), sum(agg_day$volume.sum))
     testthat::expect_equal(sum(sensor_results$occupancy, na.rm = T), sum(agg_day$occupancy.sum))
@@ -50,8 +56,9 @@ testthat::try_again(2, {
 
     # test argument checks--------------------------------------------------------
     testthat::expect_error(aggregate_sensor_data(sensor_results,
-                                                 config = config_sample,
-                                                 interval_length = 48))
+      config = config_sample,
+      interval_length = 48
+    ))
 
     testthat::expect_error(aggregate_sensor_data(rbind(
       sensor_results,

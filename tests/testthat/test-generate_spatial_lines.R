171f24521dfb8ead184296ba431d8b7909bbc5b5
Liz Roten <liz.roten@metc.state.mn.us>
2020-07-23 10:47:51 -0500

    test generate_spatial_lines()

---
+++ tests/testthat/test-generate_spatial_lines.R
@@ -0,0 +1,24 @@
+test_that("spatial lines are generated", {
+  config <- as.data.table(pull_configuration())
+
+  lines_sf <- generate_spatial_lines(config = config)
+
+  testthat::expect_true("sf" %in% class(lines_sf))
+
+  unique_corridors <- nrow(unique(config[
+    , corridor_category := ifelse(corridor_route == "I-35" & r_node_lat > 45, "I35 north of cities",
+      ifelse(corridor_route == "I-35" & r_node_lat <= 45, "I35 south of cities",
+        ifelse(corridor_route == "T.H.5" & r_node_lon < -93.3, "5 west of cities",
+          ifelse(corridor_route == "T.H.5" & r_node_lon > -93.3, "5 east of cities", "Other")
+        )
+      )
+    )
+  ][
+    , corridor_id := paste(corridor_route, corridor_dir, corridor_category, sep = "_")
+  ][
+    , .(detectors = paste(detector_name, collapse = ",")),
+    keyby = .(corridor_id)
+  ][, "corridor_id"]))
+
+  testthat::expect_true(unique_corridors - length(unique(lines_sf$corridor_id)) < 5)
+})

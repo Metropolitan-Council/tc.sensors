f8871e764c57dd0f4b7259d68dc9b89eb82f0671
Liz Roten <liz.roten@metc.state.mn.us>
2020-09-10 14:13:58 -0500

    change styling

--- R/replace_impossible.R
+++ R/replace_impossible.R
@@ -38,7 +38,14 @@ replace_impossible <- function(sensor_data,
       stop("Interval cannot exceed 24 hours.")
     }
 
-    sensor_data[, volume.sum := ifelse(volume.sum >= (interval_length * 2300), NA, volume.sum)][, occupancy.sum := ifelse(occupancy.sum >= (interval_length * 216000), NA, occupancy.sum)][, volume.sum := ifelse(volume.pct.null >= 10, NA, volume.sum)][, occupancy.sum := ifelse(occupancy.pct.null >= 10, NA, occupancy.sum)][, speed := ifelse(is.na(volume.sum), NA, speed)][, speed := ifelse(is.na(occupancy.sum), NA, speed)]
+    sensor_data[
+      , volume.sum := ifelse(volume.sum >= (interval_length * 2300), NA, volume.sum)][
+        , occupancy.sum := ifelse(occupancy.sum >= (interval_length * 216000),
+                                  NA, occupancy.sum)][
+          , volume.sum := ifelse(volume.pct.null >= 10, NA, volume.sum)][
+            , occupancy.sum := ifelse(occupancy.pct.null >= 10, NA, occupancy.sum)][
+              , speed.calc := ifelse(is.na(volume.sum), NA, speed.calc)][
+                , speed.calc := ifelse(is.na(occupancy.sum), NA, speed.calc)]
   }
 
   return(sensor_data)

210ceb7354a869a1b79a13ba1c66eb3a5bfc3526
Liz Roten <liz.roten@metc.state.mn.us>
2026-06-11 16:05:00 -0500

    more tests on different districts

--- tests/testthat/test-pull_sensor_ids.R
+++ tests/testthat/test-pull_sensor_ids.R
@@ -5,3 +5,28 @@ test_that("pull_sensor_ids works", {
   testthat::expect_true(dim(sensor_ids)[[1]] > 5000)
   testthat::expect_equal(dim(sensor_ids)[[2]], 1)
 })
+
+test_that("pull_sensor_ids works for District 6", {
+  sensor_ids <- pull_sensor_ids(district = "d6")
+
+  testthat::expect_equal(class(sensor_ids)[[1]], "data.table")
+  testthat::expect_true(dim(sensor_ids)[[1]] > 100)
+  testthat::expect_equal(dim(sensor_ids)[[2]], 1)
+})
+
+
+test_that("pull_sensor_ids works for District 1", {
+  sensor_ids <- pull_sensor_ids(district = "d1")
+
+  testthat::expect_equal(class(sensor_ids)[[1]], "data.table")
+  testthat::expect_true(dim(sensor_ids)[[1]] > 2)
+  testthat::expect_equal(dim(sensor_ids)[[2]], 1)
+})
+
+test_that("pull_sensor_ids works for metro signals", {
+  sensor_ids <- pull_sensor_ids(district = "metro_signals")
+
+  testthat::expect_equal(class(sensor_ids)[[1]], "data.table")
+  testthat::expect_true(dim(sensor_ids)[[1]] > 2)
+  testthat::expect_equal(dim(sensor_ids)[[2]], 1)
+})

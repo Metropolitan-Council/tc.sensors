94fb802a820d80c0e124bdd39c005a3380c79de1
Liz Roten <liz.roten@metc.state.mn.us>
2026-06-11 14:35:42 -0500

    change to data.table and lower threshold

--- tests/testthat/test-pull_sensor_ids.R
+++ tests/testthat/test-pull_sensor_ids.R
@@ -1,7 +1,7 @@
 test_that("pull_sensor_ids works", {
   sensor_ids <- pull_sensor_ids()
 
-  testthat::expect_equal(class(sensor_ids)[[1]], "tbl_df")
-  testthat::expect_true(dim(sensor_ids)[[1]] > 9000)
+  testthat::expect_equal(class(sensor_ids)[[1]], "data.table")
+  testthat::expect_true(dim(sensor_ids)[[1]] > 5000)
   testthat::expect_equal(dim(sensor_ids)[[2]], 1)
 })
